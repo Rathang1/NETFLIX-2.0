3215760ebf44c66e13545f2786910b2c359fa37e
Rathang1 <rathag.rajpal@gmail.com>
2023-07-31 18:11:39 -0500

    Added header nav bar

---
+++ src/Nav.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './Nav.css';
+
+function Nav() {
+  return (
+    <div className='nav nav__black'>
+        <div className="nav_contents">
+            <img className='nav_logo' src="https://upload.wikimedia.org/wikipedia/commons/7/7a/Logonetflix.png" alt=""/>
+            <img className='nav_avatar' src="https://upload.wikimedia.org/wikipedia/commons/0/0b/Netflix-avatar.png" alt=""/>
+        </div>
+    </div>
+  )
+}
+
+export default Nav
---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyCRBVqY-_DzOHtSMSrK6iAnN1yZyg7w2Ac",
+    authDomain: "netflix-clone-6b066.firebaseapp.com",
+    projectId: "netflix-clone-6b066",
+    storageBucket: "netflix-clone-6b066.appspot.com",
+    messagingSenderId: "561451384634",
+    appId: "1:561451384634:web:1d7d4066c9113337b4da03"
+  };
