fe782df75d9bf6f0c8bb16d636484fb0ec3637d0
Rathang1 <rathag.rajpal@gmail.com>
2023-08-22 15:35:32 -0500

    Added Redux Store

--- src/App.js
+++ src/App.js
@@ -1,12 +1,32 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import './App.css';
 import HomeScreen from './screens/HomeScreen';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import LoginScreen from './screens/LoginScreen';
+import { auth } from './firebase';
+import { useDispatch } from 'react-redux';
+import { login, logout } from './features/userSlice';
 
 
 function App() {
   const user = null;
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    const unsubscribe = auth.onAuthStateChanged(userAuth => {
+      if(userAuth) {
+        // Logged In
+        dispatch(login({
+          uid: userAuth.uid,
+          email: userAuth.email,
+        }))
+      } else {
+        // Logged out
+        dispatch(logout);
+      }
+    });
+    return unsubscribe;
+  }, []);
 
   return (
     <div className="app">
