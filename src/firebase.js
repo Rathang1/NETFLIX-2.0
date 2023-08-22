0e830bd7921073571f93c443411c2ca12384ec4b
Rathang1 <rathag.rajpal@gmail.com>
2023-08-22 00:31:00 -0500

    Added sign in functionality through firebase

--- src/firebase.js
+++ src/firebase.js
@@ -1,3 +1,7 @@
+import firebase from 'firebase/compat/app';
+import 'firebase/compat/auth';
+import 'firebase/compat/firestore';
+
 const firebaseConfig = {
     apiKey: "AIzaSyCRBVqY-_DzOHtSMSrK6iAnN1yZyg7w2Ac",
     authDomain: "netflix-clone-6b066.firebaseapp.com",
@@ -6,3 +10,10 @@ const firebaseConfig = {
     messagingSenderId: "561451384634",
     appId: "1:561451384634:web:1d7d4066c9113337b4da03"
   };
+
+  const firebaseApp = firebase.initializeApp(firebaseConfig);
+  const db = firebaseApp.firestore();
+  const auth = firebase.auth();
+
+  export {auth};
+  export default db; 
